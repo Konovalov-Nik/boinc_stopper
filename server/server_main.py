76ca74a012d5823a0036e38ddaf8e95148fca50c
Nikita Konovalov <nkonovalov@mirantis.com>
2018-09-13 10:01:50 +0200

    Server side

---
+++ server/server_main.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+STATE = False
+
+
+@app.route("/get", methods=["GET"])
+def get():
+    return str(STATE)
+
+
+@app.route("/set/<state>", methods=["POST"])
+def _set(state=False):
+    global STATE
+    if state in ("True", "true", "1", "yes", "enabled"):
+        STATE = True
+    else:
+        STATE = False
+
+
+http_server = WSGIServer(('', 5020), app)
+http_server.serve_forever()
